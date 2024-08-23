73958d0472bca5f131b534634a075672b4aa5183
Bikash <nayakbiku18a@gmail.com>
2024-08-24 01:00:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [principal,setPrincipal]=useState(0)
+  const [interest,setInterest]=useState(0)
+  const [year,setYear]=useState(0)
+  const [emi,setEmi]=useState(0)
+
+  const handleChange=(e)=>{
+     const id=e.target.id
+     const value=e.target.value
+     if(id==="principal"){
+      setPrincipal(value)
+     }else if(id==="interest"){
+      setInterest(value)
+     }else{
+      setYear(value)
+     }
+  }
+  //P x R x (1+R)^N / [(1+R)^N-1]
+  const calculateEmi=()=>{
+    const r=interest/12/100;
+    const x=Math.pow((1+r),(year*12));
+    const result=principal*r*(x)/(x-1);
+    setEmi(Math.round(result))
+  }
+  useEffect(()=>{
+    calculateEmi()
+  },[principal,interest,year])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>EMI Calculator</h2>
+     <div className='inputs'>
+      <p>Principal:</p>
+      <input 
+      onChange={handleChange}
+       id='principal' type="number" />
+
+      <p>Interest/year:</p>
+      <input
+       onChange={handleChange}
+        id='interest' type="number" />
+     
+      <p>Year:</p>
+      <input 
+       onChange={handleChange}
+       id='year' type="number" />
+     </div>
+     <div className="output">
+      <p>Your EMI is <span>{emi?emi:0}</span> </p>
+     </div>
     </div>
   );
 }
